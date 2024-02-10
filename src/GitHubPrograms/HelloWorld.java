4b32dc43edda2a92a687bd7f0833f3d4c81577b5
admin <admin@LAPTOP-FJ3D388H>
2024-02-10 11:43:23 +0530

    First commit

---
+++ src/GitHubPrograms/HelloWorld.java
@@ -0,0 +1,11 @@
+package GitHubPrograms;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
